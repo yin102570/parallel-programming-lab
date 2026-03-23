22fbb270f866d36d22acb30f34296e8e4abd8332
2410665-Yin Jiayi <qzasdf123@outlook.com>
2026-03-23 04:09:08 -0700

    targeted performance optimization

--- src/arr_sum_fast.cpp
+++ src/arr_sum_fast.cpp
@@ -1,19 +1,23 @@
-#include "../include/base.h"
+﻿#include "../include/base.h"
 
-TYPE arr_sum_fast(vector<TYPE> arr, int n) {
-    while (n > 1) {
-        int half = n / 4;
-        for (int i = 0; i < half; ++i) {
-            arr[i] += arr[i + half];
-            arr[i] += arr[i + 2*half];
-            arr[i] += arr[i + 3*half];
-        }
-        if (n % 4 != 0) {
-            for (int i = 4*half; i < n; ++i) {
-                arr[0] += arr[i];
-            }
-        }
-        n = (n + 3) / 4;
+
+TYPE arr_sum_fast(const vector<TYPE>&arr, int n) {
+    // 4路并行累加
+    TYPE sum0 = 0.0, sum1 = 0.0, sum2 = 0.0, sum3 = 0.0;
+    int i = 0;
+
+    // 每次处理4个元素
+    for (; i + 3 < n; i += 4) {
+        sum0 += arr[i];
+        sum1 += arr[i + 1];
+        sum2 += arr[i + 2];
+        sum3 += arr[i + 3];
     }
-    return arr[0];
+
+    // 处理剩余元素
+    for (; i < n; ++i) {
+        sum0 += arr[i];
+    }
+
+    return sum0 + sum1 + sum2 + sum3;
 }
