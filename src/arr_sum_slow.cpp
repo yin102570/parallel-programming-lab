0f6ae77b9cf941cb4f3f0b3eff5951606e2d4f8b
G10 ReqYin24 <qzasdf123@outlook.com>
2026-04-06 16:51:43 +0800

    chore: remove old matrix/array optimization code, keep only ANNS

--- src/arr_sum_slow.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/base.h"
-
-TYPE arr_sum_slow(const vector<TYPE>& arr, int n) {
-    TYPE s = 0.0;
-    for (int i = 0; i < n; ++i) {
-        s += arr[i];
-    }
-    return s;
-}
